fa42996d57392b4047e4d988b45001e093fb6145
d5ng <dong5ffice@gmail.com>
2025-01-16 12:23:26 +0900

    ✨ Feat: 나만의 리액트 진입점 추가

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-// Your code..
+// import { useState } from "./core/hooks"
+import { render } from "./core/client"
+import Counter from "./components/Counter"
+
+const rootElement = document.getElementById("app")! as HTMLDivElement
+
+render(rootElement, Counter)
