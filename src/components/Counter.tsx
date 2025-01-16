306d4bd5e708fbf17d5fe932c4ae327ea870a465
d5ng <dong5ffice@gmail.com>
2025-01-16 12:21:38 +0900

    ✨ Feat: Counter Component 추가

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { useState } from "@/core/hooks"
+import "./Counter.css"
+
+export default function Counter() {
+  const [count, setCount] = useState(1)
+
+  const handleIncrease = () => setCount(count + 1)
+  // const handleDecrease = () => setCount(count - 1)
+
+  return (
+    <div className="count-wrapper">
+      <p className="count">{count}</p>
+      {/* <button onClick={handleDecrease}>-1</button> */}
+      <button onClick={handleIncrease}>+1</button>
+    </div>
+  )
+}
