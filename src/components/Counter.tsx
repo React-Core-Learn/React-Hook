aa77089df743b44d651508b7ef1f5501b8215483
d5ng <dong5ffice@gmail.com>
2025-01-16 12:24:05 +0900

    ✨ Feat: Counter 컴포넌트 로직 추가

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,12 +5,12 @@ export default function Counter() {
   const [count, setCount] = useState(1)
 
   const handleIncrease = () => setCount(count + 1)
-  // const handleDecrease = () => setCount(count - 1)
+  const handleDecrease = () => setCount(count - 1)
 
   return (
     <div className="count-wrapper">
       <p className="count">{count}</p>
-      {/* <button onClick={handleDecrease}>-1</button> */}
+      <button onClick={handleDecrease}>-1</button>
       <button onClick={handleIncrease}>+1</button>
     </div>
   )
