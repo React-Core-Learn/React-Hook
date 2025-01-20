ad84a82094e347d48986cd7edab4e7a9e7be5a0e
d5ng <dong5ffice@gmail.com>
2025-01-20 17:40:17 +0900

    📝 Docs: Internals 내부 객체 주석 추가

--- src/core/sharedInternals.ts
+++ src/core/sharedInternals.ts
@@ -9,7 +9,7 @@ interface Internals {
 }
 
 /**
- * @todo 이 객체는 코어 객체입니다. 값을 절대로 변경하지 마세요.
+ * NOTE - 이 객체는 코어 객체입니다. 값을 절대로 변경하지 마세요. 만약 값을 변경하게되면 제대로 동작하지 않을 수 있습니다.
  */
 export const internals: Internals = {
   hooks: [],
