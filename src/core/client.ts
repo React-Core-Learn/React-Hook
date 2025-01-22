e12fc547cd1f3ba71a74972ffe76f8051941b6ba
d5ng <dong5ffice@gmail.com>
2025-01-23 00:05:59 +0900

    ✨ Feat: 렌더링 최적화 및 effect 관련 코드 추가

--- src/core/client.ts
+++ src/core/client.ts
@@ -2,6 +2,18 @@ import { updateElement } from "@/core/jsx/jsx-runtime"
 import { internals } from "./sharedInternals"
 import { Component } from "./jsx/jsx-runtime.type"
 
+function frameRunner(callback: () => void) {
+  let rafId: ReturnType<typeof requestAnimationFrame>
+
+  return () => {
+    if (rafId) {
+      cancelAnimationFrame(rafId)
+    }
+
+    rafId = requestAnimationFrame(callback)
+  }
+}
+
 export const { render, reRender } = (function () {
   const render = (element: HTMLElement, component: Component) => {
     internals.rootElement = element
@@ -9,13 +21,17 @@ export const { render, reRender } = (function () {
     reRender()
   }
 
-  const reRender = () => {
+  const reRender = frameRunner(() => {
     if (!internals.rootElement || !internals.rootComponent) return
     const newVDOM = internals.rootComponent()
+
     updateElement(internals.rootElement, newVDOM, internals.currentVDOM)
     internals.currentHookIndex = 0
     internals.currentVDOM = newVDOM
-  }
+
+    internals.effectList.filter((effectHook) => effectHook).forEach((fn) => fn())
+    internals.effectList = []
+  })
 
   return { render, reRender }
 })()
